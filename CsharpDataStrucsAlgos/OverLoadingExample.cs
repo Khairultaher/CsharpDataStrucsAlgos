eaa59f513d3d01f01ce77b168b0a8c6cdf4ebb45
Khairul Alam <kalam@ctsdatabase.com>
2025-12-08 12:36:58 +0600

    updated

---
+++ CsharpDataStrucsAlgos/OverLoadingExample.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CsharpDataStrucsAlgos;
+
+public class Calculator {
+
+    public double Product(double x, double y) {
+        return x * y;
+    }
+
+    // Overloading the function to handle three arguments
+    public double Product(double x, double y, double z) {
+        return x * y * z;
+    }
+
+    // Overloading the function to handle int
+    public int Product(int x, int y) {
+        return x * y;
+    }
+}
